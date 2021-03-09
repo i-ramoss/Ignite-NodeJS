e7ec6f18ccd543e1843c3fe40a5853197a489826
Ian_Ramos <ianramossantos@hotmail.com>
2021-03-09 16:42:51 -0300

    feat: routes of different http methods

--- Chapter-I/nodejs-fundamentals/src/index.js
+++ Chapter-I/nodejs-fundamentals/src/index.js
@@ -8,4 +8,20 @@ app.get('/', (request, response) => {
   return response.json({ welcome: 'Ignite Started! 🚀'})
 })
 
+app.get('/courses', (request, response) => {
+  return response.json(["Course 1", "Course 2", "Course 3"])
+})
+
+app.post('/courses', (request, response) => {
+  return response.json(["Course"])
+})
+
+app.put('/courses/:id', (request, response) => {
+  return response.json(["Course 9", "Course 2", "Course 8", "Course 4"])
+})
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json(["Course 9", "Course 8", "Course 4"])
+})
+
 app.listen(PORT, () => console.log(`🔥 Server started at http://localhost:${PORT}`))

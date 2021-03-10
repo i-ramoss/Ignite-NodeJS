9d27cd9b74ab6356c279ee049ca16d44e542c87d
Ian Ramos <ianramossantos@hotmail.com>
2021-03-10 10:14:25 -0300

    feat: crate account and validate existing account

--- Chapter-I/first-nodejs-project/src/index.js
+++ Chapter-I/first-nodejs-project/src/index.js
@@ -1,7 +1,29 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const PORT = 3333
 
 const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+app.post('/account', (request, response) => {
+  const { name, cpf } = request.body
+
+  const customerAlreadyExists = customers.some( customer => customer.cpf === cpf)
+
+  if (customerAlreadyExists) return response.status(400).json({ error: 'Customer already exists!' })
+
+  customers.push({
+    id: uuidv4(),
+    name,
+    cpf,
+    statement: []
+  })
+
+  return response.status(201).send(customers)
+})
 
 .listen(PORT, () => console.log(`🔥 Server started at http://localhost:${PORT}`))

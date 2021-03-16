6674049496b2c9f2f756d4c1c484d09255292f4f
Ian Ramos <ianramossantos@hotmail.com>
2021-03-16 09:56:13 -0300

    config: debug in the application

--- Chapter-II/rentalx/src/server.ts
+++ Chapter-II/rentalx/src/server.ts
@@ -4,8 +4,16 @@ const PORT = 3333;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   response.json({ message: 'Hi TypeScript!' });
+});
+
+app.post('/course', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 });
 
 app.listen(PORT, () => console.log(`🔥️ Server is running at port ${PORT}`));

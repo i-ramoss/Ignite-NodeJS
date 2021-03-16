339f98a6dde7c2a8d383d660e4b1d7874df12ab9
Ian Ramos <ianramossantos@hotmail.com>
2021-03-16 09:35:44 -0300

    config: add ts-node-dev

--- Chapter-II/rentalx/src/server.ts
+++ Chapter-II/rentalx/src/server.ts
@@ -4,4 +4,8 @@ const PORT = 3333;
 
 const app = express();
 
+app.get('/', (request, response) => {
+  response.json({ message: 'Hi TypeScript!' });
+});
+
 app.listen(PORT, () => console.log(`🔥️ Server is running at port ${PORT}`));

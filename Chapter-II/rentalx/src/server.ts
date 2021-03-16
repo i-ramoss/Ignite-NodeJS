123a81d67b4cbb8e1e889bfee025f63bbbfeaf4b
Ian Ramos <ianramossantos@hotmail.com>
2021-03-16 10:10:59 -0300

    feat: create route of category recourse

--- Chapter-II/rentalx/src/server.ts
+++ Chapter-II/rentalx/src/server.ts
@@ -1,19 +1,13 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const PORT = 3333;
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hi TypeScript!' });
-});
-
-app.post('/course', (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(PORT, () => console.log(`🔥️ Server is running at port ${PORT}`));

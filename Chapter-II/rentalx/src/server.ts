eece0ae784a613b3fc34f18a4dff05d43d7571ed
Ian Ramos <ianramossantos@hotmail.com>
2021-03-18 19:33:20 -0300

    feat: route and service to create a new specification

--- Chapter-II/rentalx/src/server.ts
+++ Chapter-II/rentalx/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const PORT = 3333;
 
@@ -9,5 +10,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/categories', categoriesRoutes);
+app.use('/specifications', specificationsRoutes);
 
 app.listen(PORT, () => console.log(`🔥️ Server is running at port ${PORT}`));
